549adf95373d51b90f113de60136e013466094a1
Matias Rodriguez <matiasrodriguezok@gmail.com>
2024-07-04 16:54:25 -0300

    2 commit

---
+++ js/main.js
@@ -0,0 +1,43 @@
+
+let listaDeTareas = [];
+
+function agregarTarea() {
+    let tarea = prompt("Ingrese la tarea:");
+    listaDeTareas.push({ tarea: tarea, completada: false });
+}
+
+function mostrarTareas() {
+    if (listaDeTareas.length === 0) {
+        console.log("La lista de tareas está vacía.");
+    } else {
+        console.log("Lista de tareas para el viaje:");
+        for (let i = 0; i < listaDeTareas.length; i++) {
+            let item = listaDeTareas[i];
+            console.log(`${i + 1}. ${item.tarea}`);
+        }
+    }
+}
+function marcarComoCompletada() {
+    if (listaDeTareas.length === 0) {
+        console.log("No hay tareas para marcar como completadas.");
+        return;
+    }
+    
+    let tareaCompletar = prompt("Ingrese el nombre de la tarea a marcar como completada:");
+    let tareaEncontrada = false;
+
+    for (let i = 0; i < listaDeTareas.length; i++) {
+        let item = listaDeTareas[i];
+        if (item.tarea === tareaCompletar) {
+            item.completada = true;
+            tareaEncontrada = true;
+            console.log(`Tarea "${tareaCompletar}" marcada como completada.`);
+            break;
+        }
+    }
+    
+    if (!tareaEncontrada) {
+        console.log(`No se encontró la tarea "${tareaCompletar}" en la lista.`);
+    }
+}
+
